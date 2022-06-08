b7d112b5f0474e9d01e03a8c5d202497e074230c
Vitali Kasaty <vitalik007007@gmail.com>
2022-06-08 20:47:07 +0300

    component Model, add and get parameters

--- src/main/java/com/vitalikasaty/spring/mvc/MyController.java
+++ src/main/java/com/vitalikasaty/spring/mvc/MyController.java
@@ -1,6 +1,9 @@
 package com.vitalikasaty.spring.mvc;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,8 +19,23 @@ public class MyController {
 		return "ask-emp-details-view";
 	}
 	
+//	@RequestMapping("/showDetails")
+//	public String showEmpDetails() {
+//		return "show-emp-details-view";
+//	}
+
 	@RequestMapping("/showDetails")
-	public String showEmpDetails() {
+	public String showEmpDetails(HttpServletRequest request, Model model) {
+		
+		String empName = request.getParameter("employeeName"); //Реквестом получаем значение по имени параметра
+		empName = "Mr. " + empName;
+		model.addAttribute("nameAttribute", empName); 
+		//Model - это контейнер для хранения любых данных
+		//"nameAttribute" - это имя атрибута по которому будем обращаться для получения значения
+		//привязанного к нему в Model
+		
+		model.addAttribute("description", " - Java developer");
+		
 		return "show-emp-details-view";
 	}
 
